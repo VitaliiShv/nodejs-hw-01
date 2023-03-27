ea480dafef2e1fef93180544cb2c88930c7d299f
VitaliiShv <vitalyashved@gmail.com>
2023-03-27 23:41:54 +0300

    add proj

---
+++ README.md
@@ -0,0 +1,1 @@
+https://monosnap.com/list/6421f712782734af254911aa
---
+++ index.js
@@ -0,0 +1,39 @@
+const { program } = require("commander");
+
+const contacts = require("./db");
+
+const invokeAction = async ({ action, id, phone, name, email }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const contact = await contacts.getContactById(id);
+      return console.log(contact);
+
+    case "add":
+      const newContact = await contacts.addContact(phone, name, email);
+      return console.log(newContact);
+
+    case "remove":
+      const removedContact = await contacts.removeContact(id);
+      return console.log(removedContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const argv = program.opts();
+
+invokeAction(argv);
